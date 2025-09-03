da22aef4d40c1d6ef92c43903b95ccd037b16d82
NateThan420 <jerkfacegalimba2@gmail.com>
2025-09-03 22:42:51 +0800

    revised logout button

--- logout.js
+++ logout.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const logoutBtn = document.getElementById('logoutBtn');
 
     logoutBtn.addEventListener('click', () => {
-        // Redirect the user to the login page
         window.location.href = 'index.html';
     });
 });
